ae3f270a8c919164ed003430904d90552f8cf3bc
Mohammed Adam <mohanadam2233@gmail.com>
2025-08-24 14:09:16 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ export function Hero() {
           <div className="relative">
             <div className="absolute -inset-1 rounded-full bg-gradient-to-r from-indigo-500 to-cyan-500 blur opacity-60" />
             <img
-              src="https://unsplash.com/photos/close-up-portrait-of-smiling-handsome-man-in-round-glasses-and-blue-shirt-isolated-on-gray-textured-wall-EckYAt589SY" 
+              src="https://plus.unsplash.com/premium_photo-1682096252599-e8536cd97d2b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
               alt="Profile"
               className="relative rounded-full w-48 h-48 md:w-64 md:h-64 object-cover border-4 border-white/20 shadow-2xl"
               loading="lazy"
